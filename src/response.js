2c78f513e3c5083482c42232f1b1a0047ca58846
James <redbarcompany+render@gmail.com>
2026-07-18 22:56:46 -0700

    initial package commit

---
+++ src/response.js
@@ -0,0 +1,21 @@
+console.log("====== Welcome to James api response =======");
+console.log("====== Response Generation Started =======");
+class Response{
+    success(message, data = null){
+        return {
+            success: true,
+            message,
+            data
+        };
+    }
+
+    error(message, errors = null){
+        return {
+            success: false,
+            message,
+            // errors
+        }
+    }
+}
+
+module.exports = new Response();
